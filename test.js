23d8ae348bf50d4268cb5b5971f047f8ce67f1d3
Terren Peterson <terrenjpeterson@yahoo.com>
2015-07-14 22:34:23 +0000

    added comments

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-// pull in libraries to create http server
+// pull in appropriate nodejs libraries to create http server and read filesystems
 var http = require('http');
 var fs = require('fs');
 
-//
+// basic logging to alert that the server works
 console.log("Server Now Starting...");
 
-//
+// read the file into memory of the app server
 var home_page = fs.readFileSync("index.html", 'utf8');
 
-// start the server
+// start the app server and respond to requests by serving back the file read into the app server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-//  response.end("Hello World\n");
   response.end(home_page);
   console.log("Page just hit.");
 });
