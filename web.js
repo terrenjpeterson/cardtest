7a68d24c9dca5f6340081db2140693ed4683b639
Terren Peterson <terrenjpeterson@yahoo.com>
2015-08-01 14:52:38 +0000

    reformatted packaging

---
+++ web.js
@@ -0,0 +1,32 @@
+// pull in appropriate nodejs libraries to create http server and read filesystems
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+// basic logging to alert that the server works
+console.log("Server Now Starting...");
+
+// start the app server and respond to requests by serving back the file read into the app server
+var app = express();
+
+var server = http.createServer(app);
+
+// read the file into memory of the app server
+var home_page = fs.readFileSync("index.html", 'utf8');
+
+// this gets static files linked so that they may be served in get requests
+app.use('/js', express.static(__dirname + '/js'));
+
+// respond to http requests
+app.get('/', function(req, res){
+    res.send(home_page);
+    console.log("Page just hit.");
+});
+
+app.get('/index.html', function(req, res) {
+   res.send(home_page);
+   console.log("Page just hit.");
+});
+
+// start the process of listening on a port
+server.listen(8080);
